5e29dd633a8777eb0425ea5a6e19356b5f8e626e
ati <atirekg@gmail.com>
2012-09-10 22:27:54 +0530

    adding project dir

---
+++ src/org.thoughtworks.example.build/MyFirstAntPro.java
@@ -0,0 +1,12 @@
+package org.thoughtworks.example.build;
+
+public class MyFirstAntPro {
+
+    public static void main(String args[]){
+
+        for(int i=0; i<args.length;i++){
+//            System.out.println(args[i]);
+            System.out.println("hi jksdc ajksnkjas");
+        }
+    }
+}
